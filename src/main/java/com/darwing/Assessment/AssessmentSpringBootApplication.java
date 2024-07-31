4683cfcf6d165eee6e44fac8a34d7bf86af11218
Darwing <Darwingx990@gmail.com>
2024-07-31 09:11:52 -0500

    Creating repo

---
+++ src/main/java/com/darwing/Assessment/AssessmentSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.darwing.Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentSpringBootApplication.class, args);
+	}
+
+}
